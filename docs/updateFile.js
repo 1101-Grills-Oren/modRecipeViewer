43e4a8cc6cec71f33f2747cd069cae5689ab8212
Nero <134662565+1101-Grills-Oren@users.noreply.github.com>
2025-06-11 10:24:24 -0700

    Create updateFile.js

---
+++ docs/updateFile.js
@@ -0,0 +1,36 @@
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+  auth: 'YOUR-TOKEN'
+})
+get=function(owner,repo,path){
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+
+
+return await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+  owner: owner,
+  repo: repo,
+  path: path,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+});
+}  
+set=function(owner,repo,path,content){
+  
+  await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+  owner: owner,
+  repo: repo,
+  path: path,
+  message: 'a commit',
+  committer: {
+    name: '???',
+    email: '???'
+  },
+  content: btoa(content),
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+}
